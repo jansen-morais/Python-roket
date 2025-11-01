089ab44682ee605ca7af804ed3abbba2350102ff
Jansen Morais <jansenmorais86@gmail.com>
2025-11-01 01:52:50 +0000

    teste da função

---
+++ modelos/imprimir_dados.py
@@ -0,0 +1,11 @@
+def imprimir_dados(numero, texto): # Passo 1: Definir a função imprimir_dados
+    print("Númeror:", numero)
+    print("Texto", texto)
+
+imprimir_dados(10, "ola, mundo") # Passo 2: Chamar a função imprimir_dados com dois argumentos
+'''O exemplo demonstrou como os argumentos (um número e uma string) 
+são vinculados aos parâmetros (número e texto) da função imprimir_dados. 
+Quando a função é chamada com os valores 10 e "Olá, mundo!", 
+esses valores são vinculados aos parâmetros dentro da função, 
+resultando na impressão do número e da string fornecidos. 
+Isso exemplifica o conceito de binding em Python.'''
