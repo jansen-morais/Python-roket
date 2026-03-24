60f1ccf0d92ce13cd08be2047fbf36af1dd63db2
Jansen Morais <jansenmorais86@gmail.com>
2026-03-24 16:40:00 +0000

    manipulação de diretorios

---
+++ estudos/manipulação-de-dados/crindo-diretorio.py
@@ -0,0 +1,35 @@
+import os
+
+def porcecessar_arquivo(arquivo_origem, arquivo_destino):
+    try:#comoando para tentar execultar as intruções 
+        with open(arquivo_origem, 'r', encoding='utf8') as f_origem:
+            conteudo = f_origem.read()
+    #comando parao caso de exeções 
+    except FileNotFoundError:
+        print(f'Arquivo {arquivo_origem}, não encontrado.')        
+        return
+    except PermissionError:
+        print(f'Sem perção para ler o {arquivo_origem}.')
+        return
+    except Exception as e:
+        print(f'Erro inesperado ao ler {arquivo_origem}: {e}')
+        return
+    
+    try:
+        with open(arquivo_destino, 'w+', encoding='utf8') as f_destino:
+            f_destino.write("Cabeçalho: conteudo do Arquivo\n")
+            f_destino.write(conteudo)
+    except PermissionError:
+        print(f'Sem permição para eccrever em {arquivo_destino}')
+    except Exception as e:
+        print(f'Erro inesperado ao escrever em {arquivo_destino}: {e}')
+
+def main():
+    diretorio_trabalho = "diretorio_trabalho"
+    arquivo_origem = os.path.join(diretorio_trabalho, "arquivo_origem.txt")
+    arquivo_destino = os.path.join(diretorio_trabalho, "arquivo_destino.txt")
+
+    porcecessar_arquivo(arquivo_origem, arquivo_destino)
+
+if __name__ == "__main__":
+    main()
